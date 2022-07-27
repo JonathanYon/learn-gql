cbd545a3d58e2b51011c38a2d4dd87c9af492619
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2022-07-27 15:16:20 +0200

    USER AND POST MODELS

---
+++ src/models/user.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  email: String,
+  createdAt: String,
+});
+
+export default model("User", userSchema);
