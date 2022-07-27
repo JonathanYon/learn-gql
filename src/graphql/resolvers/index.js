134d059889c6c2a318f2a54b2af12071d5aa3966
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2022-07-27 22:11:03 +0200

    .

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,10 +1,13 @@
-
+import { userResolver } from "./user.js"
 import {PostResolver} from "./post.js"
 
 
 const resolvers =  {
     Query: {
         ...PostResolver.Query
+    },
+    Mutation: {
+        ...userResolver.Mutation
     }
 }
 
