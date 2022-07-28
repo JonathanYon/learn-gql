2d7a83a53c361816af0cdd76fbd919316e7c13d7
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2022-07-28 12:25:02 +0200

    create, delete post resolvers

--- src/graphql/resolvers/post.js
+++ src/graphql/resolvers/post.js
@@ -1,15 +1,79 @@
-import posts from "../../models/post.js"
+import { AuthenticationError } from "apollo-server";
+
+import postModel from "../../models/post.js";
+import AuthMiddleware from "../../utils/authMidd.js";
 
 export const PostResolver = {
-    Query: {
-        async getPosts(){
-          try {
-            const post = await posts.find()
-            console.log('-->',post)
-            return post
-          } catch (error) {
-            throw new Error(error)
+  Query: {
+    async getPosts() {
+      try {
+        const post = await postModel.find().sort({ createdAt: -1 });
+        console.log("-->", post);
+        return post;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    async getPost(_, { postId }) {
+      try {
+        const post = await postModel.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw new Error("Post not Found");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+  Mutation: {
+    async createPost(_, { body }, ctx) {
+      try {
+        const user = await AuthMiddleware(ctx);
+        // console.log("--+", ctx);
+        console.log("---<", user);
+
+        if (user) {
+          const newPost = await postModel({
+            body,
+            user: user._id,
+            username: user.username,
+            email: user.email,
+            createdAt: new Date().toISOString(),
+          }).save();
+          return newPost;
+        } else {
+          throw new AuthenticationError("You need to register first");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+
+    async deletePost(_, { postId }, ctx) {
+      try {
+        const user = await AuthMiddleware(ctx);
+        // console.log("--+", ctx);
+        if (user) {
+          const post = await postModel.findById(postId);
+          if (post) {
+            const match = post.user.toString() === user._id.toString();
+            if (match) {
+              await post.delete();
+              return "deleted";
+            } else {
+              throw new AuthenticationError(
+                "NOT Authorized to perform this Action"
+              );
+            }
+          } else {
+            throw new Error(`Post ${postId} not found`);
           }
         }
-    }
-}
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+};
